f2de1c08452a592203946bd9b02121b49fbc1a06
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-17 16:00:34 +0600

    design home page and animate in one latter and design it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.scss'
-import Layout from './components/Layout'
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Home from './components/Home';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss';
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          hajjad
+          <br />
+          Web Developer
+        </h1>
+        <h2>Frontend Developer / JavaScript / React / NodeJS</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
