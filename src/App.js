7e028eda288c3571da4028bd6101d420aa7c673f
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-17 12:37:08 +0600

    css file add design it and initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.scss'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <h1>My Portfolio</h1>
+    </div>
+  )
 }
 
 export default App
