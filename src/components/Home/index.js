7b6c69e03e45cfb733a01fb977b5edd48f36a085
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-18 19:43:11 +0600

    about page edit and loader add and design it

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import AnimatedLetters from '../AnimatedLetters';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -65,6 +66,7 @@ const Home = () => {
         </div>
         <Logo />
       </div>
+      <Loader type="pacman" />
     </>
   );
 };
